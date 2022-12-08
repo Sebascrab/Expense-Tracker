80cbddd112aec4adb4ccd342e517f72a23d79fbd
Sebastian Tischner <alexanders.tischner@outlook.com>
2022-12-08 14:58:27 -0700

    defined methods that I am using in js

---
+++ js/BudgetTracker.js
@@ -0,0 +1,52 @@
+
+
+export default class BudgetTracker {
+    constructor(querySelectorString) {
+
+    }
+
+    // return html for table
+    static html() {
+
+    }
+
+    // return html for single row in table
+    static entryHtml() {
+
+    }
+
+    // initial loading
+    load() {
+
+    }
+
+    // take current rows and display total amount
+    updateSummary() {
+
+    }
+
+    // save to local storage
+    save() {
+
+    }
+
+    // add new entry in table
+    addEntry(entry = {}) {
+
+    }
+
+    // get all rows 
+    getEntryRows() {
+
+    }
+
+    // button to add new entry
+    onNewEntryBtnClick() {
+
+    }
+
+    // button to delete entry 
+    onDeleteEntryBtnClick(e) {
+
+    }
+}
