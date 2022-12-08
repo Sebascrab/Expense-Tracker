b9dd26d336025de2af8a469cd56d2c0192c2ae6f
Sebastian Tischner <alexanders.tischner@outlook.com>
2022-12-08 15:41:30 -0700

    added to Budget Tracker js file methods

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -1,52 +1,128 @@
-
-
 export default class BudgetTracker {
-    constructor(querySelectorString) {
+  constructor(querySelectorString) {
+    this.root = document.querySelector(querySelectorString);
+    this.root.innerHTML = BudgetTracker.html();
 
-    }
+    // listening for click event on new-entry
+    this.root.querySelector(".new-entry").addEventListener("click", () => {
+      this.onNewEntryBtnClick();
+    });
 
-    // return html for table
-    static html() {
+    // loading data from local storage
+    this.load();
+  }
 
-    }
+  // return html for table
+  static html() {
+    return `
+        <table class="budget-tracker">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="entries">
+            </tbody>
+            <tbody>
+                <tr>
+                    <td colspan="5" class="controls">
+                        <button class="new-entry" type="button">New Entry</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <td colspan="5" class="summary">
+                    <strong>Total:</strong>
+                    <span>$0.00</span>
+                </td>
+            </tfoot>
+        </table>
+                 `;
+  }
 
-    // return html for single row in table
-    static entryHtml() {
+  // return html for single row in table
+  static entryHtml() {
+    return `
+        <tr>
+        <td>
+            <input type="date" class="input input-date">
+        </td>
+        <td>
+            <input type="text" class="input input-description" placeholder="wages. bills yadya">
+        </td>
+        <td>
+            <select class="input input-type">
+                <option value="income">Income</option>
+                <option value="expense">Expense</option>
+            </select>
+        </td>
+        <td>
+           <input type="number" class="input input-amount">
+        </td>
+        <td>
+            <button type="button" class="delete-entry">&#10005;</button>
+        </td>
+    </tr>
+        `;
+  }
 
-    }
+  // initial loading
+  load() {
+    //   data coming form local storage on inital page load will be in JSON. This parses that data if there is data || if there is none, empty array
+      const entries = JSON.parse(localStorage.getItem('budget-tracker-entries') || '[]');
+            // on load adds entry to our table
+        for (const entry of entries) {
+            this.addEntery(entry);
+        }
+        // on initial load updates the summary if there is any informaiton 
+        this.updateSummary();
+  }
 
-    // initial loading
-    load() {
+  // take current rows and display total amount
+  updateSummary() {}
 
-    }
+  // save to local storage
+  save() {
+    console.log(this.getEntryRows())
+  }
 
-    // take current rows and display total amount
-    updateSummary() {
+  // add new entry in table
+  addEntry(entry = {}) {
+    //   adds function to add table row on using btn
+      this.root.querySelector('.entries').insertAdjacentHTML('beforeend', BudgetTracker.entryHtml())
 
-    }
+      const row = this.root.querySelector('.entries tr:last-of-type');
+    //   gives us entry date or current date in ISO format and using regex to give us only date. 
+      row.querySelector('.input-date').value = entry.date || new Date().toISOString().replace(/T.*/, '');
+      row.querySelector('.input-description').value = entry.description || '';
+      row.querySelector('.input-type').value = entry.type || 'income';
+      row.querySelector('.input-amount').value = entry.amount || 0;
+      row.querySelector('.delete-entry').addEventListener('click', e => {
+          this.onDeleteEntryBtnClick(e);
+      })
 
-    // save to local storage
-    save() {
+      row.querySelectorAll('.input').forEach(input => {
+          input.addEventListener('change', () => this.save());
+      });
+  };
 
-    }
+  // get all rows
+  getEntryRows() {
+      return Array.from(this.root.querySelectorAll('.entries tr'))
+  }
 
-    // add new entry in table
-    addEntry(entry = {}) {
+  // button to add new entry
+  onNewEntryBtnClick() {
+      this.addEntry();
+  }
 
-    }
+  // button to delete entry
+  onDeleteEntryBtnClick(e) {
 
-    // get all rows 
-    getEntryRows() {
-
-    }
-
-    // button to add new entry
-    onNewEntryBtnClick() {
-
-    }
-
-    // button to delete entry 
-    onDeleteEntryBtnClick(e) {
-
-    }
+    console.log('deleted');
+  }
 }
